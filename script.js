e295e9c0bca1a46989cfd4b808e1673fd7eeb11e
Talal Erhoma <137126633+itsloser7@users.noreply.github.com>
2025-06-15 20:16:00 +0300

    Update script.js
    username

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function login(event) {
   event.preventDefault();
   const user = document.getElementById('username').value;
   const pass = document.getElementById('password').value;
-  if (user === 'admin' && pass === '1234') {
+  if (user === '1234' && pass === '1234') {
     window.location.href = 'dashboard.html';
   } else {
     alert('بيانات الدخول غير صحيحة');
